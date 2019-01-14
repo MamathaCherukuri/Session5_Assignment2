9125f2965f3011efc660d834b4ae6bf77a3b50ba
MamathaCherukuri <mamatha.mba26@gmail.com>
2019-01-14 23:54:25 +0530

    Add files via upload

--- Session5_Assignment2.py
+++ Session5_Assignment2.py
@@ -29,6 +29,12 @@ div(1,1)
 div(1,0)
 
 
+# In[11]:
+
+
+div(5,0)
+
+
 # 2.Implement a Python program to generate all sentences where subject is in ["Americans", "Indians"] and verb is in ["Play", "watch"] and the object is in ["Baseball","cricket"]. 
 # Hint: Subject,Verb and Object should be declared in the program as shown below. 
 # subjects=["Americans ","Indians"] verbs=["play","watch"] objects=["Baseball","Cricket"] 
